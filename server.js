a7da2f11627fc2a429bf8131d9f7f47a2a8cd9ec
Vaishnavi Ranga <24wh1a05j3@bvrithyderabad.edu.in>
2026-07-03 14:47:33 +0530

    error

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const port =process.env.PORT || 3002;
+const PORT =process.env.PORT || 3000;
          app.use(express.static('frontend'));      
 app.listen(port, () => {
     console.log(`Success! Server is running on http://localhost:${port}`);
 });
-const port =process.env.PORT || 3000;
