b5e5d75e587f1c429a66b43a3db5f106907b6cef
Vaishnavi Ranga <24wh1a05j3@bvrithyderabad.edu.in>
2026-07-03 13:44:33 +0530

    update

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const port =process.env.PORT || 3002;
          app.use(express.static('frontend'));      
 app.listen(port, () => {
     console.log(`Success! Server is running on http://localhost:${port}`);
 });
+const port =process.env.PORT || 3000;
