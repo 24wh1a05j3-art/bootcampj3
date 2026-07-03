52a5332017ce1ab1af09d5acaf9bec8e73ef300e
Vaishnavi Ranga <24wh1a05j3@bvrithyderabad.edu.in>
2026-07-03 12:10:23 +0530

    web server - setup

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+         app.use(express.static('frontend'));      
+app.listen(port, () => {
+    console.log(`Success! Server is running on http://localhost:${port}`);
+});
